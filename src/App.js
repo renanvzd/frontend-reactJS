6695e0f3611e6cdcf8d3a16141e76cbfc911a4be
Renan Veronez Drechsler <renanvz@outlook.com>
2020-11-03 19:36:40 -0300

    Function handleAddProject - console.log(projects)

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import React from 'react';
 import Header from './components/Header';
 
 function App() {
+    const projects = ['Desenvolvimento de app', 'Front-end web'];
+
+    function handleAddProject() {
+        projects.push(`Novo projeto ${Date.now()}`);
+
+        console.log(projects);
+    }
+ 
     return (
         <>
-            <Header title="Homepage">
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                </ul>
-            </Header>
-            <Header title="Projects">
-                <ul>
-                    <li>Project 1</li>
-                    <li>Project 2</li>
-                </ul>
-            </Header>
+         <Header title="Homepage" />
+            <ul>
+                {projects.map(project => 
+                    <li key={project} >{project}</li>
+                )}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar projeto </button>
         </>
     );
 }

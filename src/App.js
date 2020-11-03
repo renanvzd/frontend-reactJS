9873e7be8f23903a5b07046c3b725303ce5a44f7
Renan Veronez Drechsler <renanvz@outlook.com>
2020-11-03 16:39:37 -0300

    Reusable component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import Header from './components/Header';
+
 function App() {
-    return <h1>Hello GoStack</h1>;
+    return (
+        <>
+            <Header />
+            <Header />
+        </>
+    );
 }
 
 export default App;
